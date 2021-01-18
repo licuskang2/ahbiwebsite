2a883e9e12a46385a8479b1dcfb94f2cf64dbada
macpro-2 <isa-firdaus@ambankgroup.com>
2021-01-18 23:33:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ahbiLogo from './ahbi.JPG'; 
+import dajieLogo from './dajie.jpg'; 
+import xxLogo from './xx.png'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={ahbiLogo} className="App-logo" alt="logo" /> */}
+        <img src={xxLogo} className="App-logo" alt="logo" />
+        <img src={dajieLogo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! I'm Ah bi :D
         </p>
         <a
           className="App-link"
